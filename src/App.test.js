a05bdb887763fec6e4bdfa65e00ba0990d510a1d
Debbie <dbarros64@gmail.com>
2021-02-20 12:22:44 -0800

    adding files to github pages

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import CardList from './Components/CardList/CardList';
 
 test('renders learn react link', () => {
   render(<App />);
